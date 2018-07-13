0a6d1bf13748b21fe6058dcb766cb4bf94be6706
Fordman <fordman@MacBook-Pro-de-Fordman.local>
2018-07-13 17:03:46 +0200

    Watch lastTweet and display text via pixi text && prepare particle method

--- src/components/views/Scene.vue
+++ src/components/views/Scene.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="page-scene">
-    <p>{{lastTweet}}</p>
   </div>
 </template>
 
 <script>
+  import * as PIXI from 'pixi.js'
   export default {
     data () {
       return {
@@ -18,13 +18,65 @@
           let lastTweet = lastData.tweet.text
           if (lastTweet !== this.lastTweet) {
             this.lastTweet = lastTweet
-            console.log(this.lastTweet)
           }
         })
+      },
+      initPixi () {
+        const container = document.querySelector('.page-scene')
+        let width = window.innerWidth
+        let height = window.innerHeight
+        const resolution = window.devicePixelRatio
+
+        const app = new PIXI.Application(width, height, {
+          resolution: resolution,
+          autoResize: true,
+          transparent: true,
+          antialias: true,
+          forceFXAA: true
+        })
+        container.appendChild(app.view)
+        var textSample = new PIXI.Text('The tweets \n are coming!', {
+          fontFamily: 'Helvetica',
+          fontSize: 14,
+          lineHeight: 16,
+          fill: 'black',
+          align: 'left'
+        })
+        this.$watch('lastTweet', (tweet) => {
+          textSample.text = tweet
+        })
+        textSample.position.set(20)
+        app.stage.addChild(textSample)
+        createParticle(20, 20, 20)
+
+        function createParticle (x, y, radius) {
+          var graphics = new PIXI.Graphics()
+          graphics.beginFill(0xe74c3c)
+          graphics.drawCircle(x, y, radius)
+          graphics.endFill()
+          graphics.interactive = true
+          graphics.buttonMode = true
+          graphics.hitArea = new PIXI.Circle(x, y, radius)
+          graphics.click = function (e) {
+            console.log(this, e)
+          }
+          // Add the graphics to the stage
+          app.stage.addChild(graphics)
+        }
       }
     },
-    created () {
+    mounted () {
+      this.initPixi()
       this.getLastTweet()
     }
   }
 </script>
+<style lang="stylus">
+  canvas {
+    position absolute
+    top 0
+    left 0
+    right 0
+    bottom 0
+  }
+</style>
