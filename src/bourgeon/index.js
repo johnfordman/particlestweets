b8c4427ec98d87bc5f7a0ccd0e85195b3cb4c8d9
Fordman <fordman@MacBook-Pro-de-Fordman.local>
2018-07-13 13:18:09 +0200

    Remove bourgeon icon

--- src/bourgeon/index.js
+++ src/bourgeon/index.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-unused-vars */
-import Icon from 'bourgeon/Icon.vue'
 import Router from 'bourgeon/router'
 
 export default {
   install (Vue, options) {
     Vue.use(Router)
-    Vue.component('icon', Icon)
   }
 }
